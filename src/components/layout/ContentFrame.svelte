a11491cdbda21d799cde1ee6a5c96beba2b95f3f
Jordan Yoon-Buck <yoonbuck@uw.edu>
2020-11-11 14:52:42 -0800

    Add layout components

---
+++ src/components/layout/ContentFrame.svelte
@@ -0,0 +1,10 @@
+<style lang="scss">
+  .content-frame {
+    margin: 0;
+    padding: 1rem;
+  }
+</style>
+
+<div class="content-frame">
+  <slot />
+</div>
---
+++ src/components/layout/HorizontalScrollArea.svelte
@@ -0,0 +1,17 @@
+<style lang="scss">
+  .outer {
+    margin: 0 -1rem;
+    overflow-y: auto;
+  }
+
+  .inner {
+    padding: 1rem;
+    width: max-content;
+  }
+</style>
+
+<div class="outer">
+  <div class="inner">
+    <slot />
+  </div>
+</div>
