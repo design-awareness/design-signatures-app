f1d1c35e59944c3798bc31ad1c2c02210282c4de
Jordan Yoon-Buck <yoonbuck@uw.edu>
2020-12-29 21:16:29 -0600

    First attempt at a service worker :)

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import preprocess from "svelte-preprocess";
+import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
 import ts from "typescript";
 
@@ -34,56 +35,79 @@ function serve() {
   };
 }
 
-export default {
-  input: "src/main.ts",
-  output: {
-    sourcemap: !production,
-    format: "iife",
-    name: "app",
-    file: "public/build/bundle.js",
-  },
-  plugins: [
-    svelte({
-      preprocess: preprocess({
-        moduleResolution: ts.ModuleKind.Node,
+export default [
+  {
+    input: "src/main.ts",
+    output: {
+      sourcemap: !production,
+      format: "iife",
+      name: "app",
+      file: "public/build/bundle.js",
+    },
+    plugins: [
+      svelte({
+        preprocess: preprocess({
+          moduleResolution: ts.ModuleKind.Node,
+        }),
+        // enable run-time checks when not in production
+        dev: !production,
+        // we'll extract any component CSS out into
+        // a separate file - better for performance
+        css: (css) => {
+          css.write("bundle.css");
+        },
       }),
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
-      css: (css) => {
-        css.write("bundle.css");
-      },
-    }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: ["svelte"],
-    }),
-    commonjs(),
-    typescript({
-      sourceMap: !production,
-      inlineSources: !production,
-    }),
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ["svelte"],
+      }),
+      commonjs(),
+      typescript({
+        sourceMap: !production,
+        inlineSources: !production,
+      }),
 
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
+      // In dev mode, call `npm run start` once
+      // the bundle has been generated
+      !production && serve(),
 
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload("public"),
+      // Watch the `public` directory and refresh the
+      // browser on changes when not in production
+      !production && livereload("public"),
 
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser(),
-  ],
-  watch: {
-    clearScreen: false,
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      production && terser(),
+    ],
+    watch: {
+      clearScreen: false,
+    },
   },
-};
+  {
+    input: "src/service-worker.ts",
+    output: {
+      sourcemap: !production,
+      format: "iife",
+      name: "workbox",
+      file: "public/service-worker.js",
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        sourceMap: !production,
+        inlineSources: !production,
+      }),
+      replace({
+        "process.env.NODE_ENV": production ? "'production'" : "'development'",
+      }),
+      production && terser(),
+    ],
+  },
+];
