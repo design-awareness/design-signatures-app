50f181df36234096704d0df17c418fffac7d6fea
khajor <kyji8art@gmail.com>
2021-06-30 17:50:50 -0700

    Worked on desing of collapsable cards on Reflection Qusstions Page

--- src/routes/ReflectionQuestions.svelte
+++ src/routes/ReflectionQuestions.svelte
@@ -2,7 +2,8 @@
     import BackButton from "../components/BackButton.svelte";
     import ContentFrame from "../components/layout/ContentFrame.svelte";
     import Header from "../components/type/Header.svelte";
-    import { CollapsibleCard } from 'svelte-collapsible'
+    import { CollapsibleCard } from 'svelte-collapsible';
+    import { reflectionQuestions } from '../data/reflection.ts';
 
 </script>
 
@@ -13,6 +14,19 @@
     </div>
     <Header>Reflection Questions</Header>
     <p>Once you record your design process, you can use this page as a tool to reflect on your process.</p>
+
+    <div class="ref-qs-container">
+      {#each reflectionQuestions as {name, questions}}
+        <CollapsibleCard>
+            <p slot='header' class="cat-name">
+              {name}
+            </p>
+            <div slot='body' class="qeustions">
+              {questions}
+            </div>
+        </CollapsibleCard>
+      {/each}
+    </div>
   </ContentFrame>
 </main>
 
@@ -20,4 +34,10 @@
   @import "src/styles/tokens";
   @import "src/styles/type";
 
+  .cat-name{
+    border-radius: 1rem 1rem 0 0;
+  }
+  .qeustions{
+    border-radius: 0 0 1rem 1rem;
+  }
 </style>
