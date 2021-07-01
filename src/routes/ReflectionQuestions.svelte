796634b130cec25fa9439cb6a8d173a2075f2685
Jordan Yoon-Buck <yoonbuck@uw.edu>
2021-06-30 23:49:09 -0700

    feat: put reflection questions into lists

--- src/routes/ReflectionQuestions.svelte
+++ src/routes/ReflectionQuestions.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import BackButton from "../components/BackButton.svelte";
-    import ContentFrame from "../components/layout/ContentFrame.svelte";
-    import Header from "../components/type/Header.svelte";
-    import { CollapsibleCard } from 'svelte-collapsible';
-    import { reflectionQuestions } from '../data/reflection.ts';
-
+  import BackButton from "../components/BackButton.svelte";
+  import ContentFrame from "../components/layout/ContentFrame.svelte";
+  import Header from "../components/type/Header.svelte";
+  // FIXME: can we get typings for this?
+  import { Accordion, AccordionItem } from "svelte-collapsible";
+  import { reflectionQuestions } from "../data/reflection";
 </script>
 
 <main class="device-frame page">
@@ -13,20 +13,27 @@
       <BackButton href="/" />
     </div>
     <Header>Reflection Questions</Header>
-    <p>Once you record your design process, you can use this page as a tool to reflect on your process.</p>
+    <p>
+      Once you record your design process, you can use this page as a tool to
+      reflect on your process.
+    </p>
 
-    <div class="ref-qs-container">
-      {#each reflectionQuestions as {name, questions}}
-        <CollapsibleCard>
-            <p slot='header' class="cat-name">
-              {name}
-            </p>
-            <div slot='body' class="qeustions">
-              {questions}
-            </div>
-        </CollapsibleCard>
+    <Accordion>
+      {#each reflectionQuestions as { name, questions }}
+        <AccordionItem key={name}>
+          <h2 slot="header" class="cat-name">
+            {name}
+          </h2>
+          <div slot="body" class="questions">
+            <ul>
+              {#each questions as question}
+                <li>{question}</li>
+              {/each}
+            </ul>
+          </div>
+        </AccordionItem>
       {/each}
-    </div>
+    </Accordion>
   </ContentFrame>
 </main>
 
@@ -34,10 +41,10 @@
   @import "src/styles/tokens";
   @import "src/styles/type";
 
-  .cat-name{
+  .cat-name {
     border-radius: 1rem 1rem 0 0;
   }
-  .qeustions{
+  .questions {
     border-radius: 0 0 1rem 1rem;
   }
 </style>
