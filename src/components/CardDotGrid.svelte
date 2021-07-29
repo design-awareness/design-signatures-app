48c6910d64fe5f8a19d1070dc326c42f01d22508
Jordan Yoon-Buck <yoonbuck@uw.edu>
2021-07-29 11:25:27 -0700

    chore(CardDotGrid): remove extra console.logs

--- src/components/CardDotGrid.svelte
+++ src/components/CardDotGrid.svelte
@@ -48,7 +48,6 @@
 
   let [grid] = makeCanvasAction(() => {
     function draw(ctx: CanvasRenderingContext2D, w: number, h: number) {
-      console.log("draw!");
       let dw = (w - PADDING_PX * 2) / ENTRIES_TO_SHOW;
       let dh = (h - PADDING_PX * 2) / activityCount;
       let maxRad = Math.min(dw, dh) / 2;
@@ -56,7 +55,6 @@
         let x = PADDING_PX + (0.5 + i) * dw;
         entry.data.forEach(({ value }, j) => {
           if (value) {
-            console.log("drawing", i, j);
             let y = PADDING_PX + (0.5 + j) * dh;
             let rad = Math.sqrt(value / maxValue) * maxRad;
             ctx.beginPath();
