add49c4fbfd97c710db5488e996fe2259cf13972
Jordan Yoon-Buck <jordan@yoonbuck.com>
2021-08-15 19:30:44 -0700

    feat: start Tutorials page

--- src/routes/about/Tutorials.svelte
+++ src/routes/about/Tutorials.svelte
@@ -6,12 +6,36 @@
   import ContentFrame from "../../components/layout/ContentFrame.svelte";
   import BackButton from "../../components/BackButton.svelte";
   import Header from "../../components/type/Header.svelte";
+  // FIXME: can we get typings for this?
+  import { Accordion } from "svelte-collapsible";
+  import AccordionItem from "../../components/AccordionItem.svelte";
 </script>
 
 <main class="device-frame page">
   <ContentFrame>
     <BackButton href="/" />
     <Header>Tutorials</Header>
+
+    <Accordion>
+      <AccordionItem label="Realtime tracking">
+        <p>
+          Realtime tracking allows you to track your process in realtime as
+          you’re designing. This mode is recommended for shorter-term projects.
+        </p>
+      </AccordionItem>
+      <AccordionItem label="Asynchronous tracking">
+        <p>
+          Asynchronous tracking allows you to track your process by entering the
+          times you spent in each design activity after you have engaged in that
+          activity (asynchronously). You can choose to enter your data either
+          daily or weekly. This mode is recommended for tracking more complex,
+          long-term projects.
+        </p>
+      </AccordionItem>
+      <AccordionItem label="Creating a design model">
+        <p />
+      </AccordionItem>
+    </Accordion>
   </ContentFrame>
 </main>
 
