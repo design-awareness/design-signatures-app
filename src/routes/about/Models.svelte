8768d1acb23dca0dd1dc57998b4b9ca22b6c3285
Jordan Yoon-Buck <jordan@yoonbuck.com>
2021-11-08 20:09:39 -0800

    about/Models: move link to research page into model detail cards
    This message & link was moved from below the Dubberly section underneath
    all of the models to a new spot at the bottom of each model's card,
    under the citation(s) and more prominent "learn more" link, if it exists.

--- src/routes/about/Models.svelte
+++ src/routes/about/Models.svelte
@@ -50,6 +50,11 @@
                 Learn more about this model
               </a>
             {/if}
+            <p class="card-smaller">
+              You can {#if description.moreInfoURL}also {/if}learn more about
+              the research behind these models on the
+              <Link href="/about/research">Design Process Research</Link> page.
+            </p>
           </AccordionItem>
         {/if}
       {/each}
@@ -64,12 +69,6 @@
       by Hugh Dubberly presents an inspirational collection of over 100 descriptions
       of design and development processes, from architecture, industrial design,
       mechanical engineering, quality management, and software development.
-    </p>
-
-    <p>
-      You can also learn more about the research behind these models on the
-      <Link href="/about/research">Design Process Research</Link>
-      page.
     </p>
   </ContentFrame>
 </main>
@@ -100,5 +99,10 @@
     display: flex;
     flex-direction: column;
     gap: 0.25rem;
+  }
+
+  .card-smaller {
+    @include type-style($type-detail);
+    color: $text-secondary-color;
   }
 </style>
